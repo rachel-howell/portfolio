88c0f0a0c59f3aeca8b80c071d6ab521751aff50
rachel-howell <rhowell96@gmail.com>
2023-04-17 17:40:44 -0700

    Add work experience

--- client/src/components/WorkExperience.jsx
+++ client/src/components/WorkExperience.jsx
@@ -1,19 +1,31 @@
 import React from 'react'
 
 const WorkExperience = () => {
+
+  const logoStyle = {
+    height: '12rem'
+  }
+
   return (
-    <div className="flex flex-col items-center h-screen w-screen mt-16">
+    <div className="flex flex-col items-center h-fit w-screen mt-16">
     <img src={require('../assets/sunriseblack.png')} alt="sunrise" width="100"/>
 
-    <p className="text-6xl font-serif mt-3 text-[#414141]">Work Experience</p>
+    <p className="text-6xl font-serif mt-3 text-[#414141] text-center">Work Experience</p>
     <div className="flex flex-row w-screen mt-16 justify-around h-1/2">
 
       <div className="flex flex-col w-80 border-2 rounded-md items-center p-4">
-        <div className="h-1/3  flex items-center">
-          <img src={require('../assets/rswhite.png')} alt="profile pic of Rachel"  width="175px" className="basis-1/3"/>
+        <div className="h-1/3  flex items-center" style={logoStyle} >
+          <img src={require('../assets/rswhite.png')} alt="rswatchworks logo" width="175px" className="basis-1/3"/>
         </div>
+
+        <div className="text-center mt-1">
+          <p className="text-2xl font-serif">Software Developer & Digital Marketing Manager</p>
+          <p className="text-sm font-mono">RSWatchWorks | Vancouver, WA</p>
+          <p className="text-xs font-mono">4/2021 - present</p>
+        </div>
+
         
-        <div className="basis-2/3">
+        <div className="basis-2/3 mt-3">
           <li>Developed and maintain custom Shopify storefront for high-growth start-up</li>
           <li>Design and launch ad campaigns - increased audience reach by 600% to date</li>
           <li>Create polished social media content to drive sales</li>
@@ -22,23 +34,37 @@ const WorkExperience = () => {
 
 
       <div className="flex flex-col w-80 items-center p-4 border-2 rounded-md">
-        <div className="h-1/3 flex items-center">
+        <div className="h-1/3 flex items-center " style={logoStyle} >
           <img src={require('../assets/varsity.png')} alt="profile pic of Rachel"  height="100px" width="100px"/>
         </div>
+
+        <div className="text-center mt-1">
+          <p className="text-2xl font-serif">Math Tutor & Academic Coach</p>
+          <p className="text-sm font-mono">Varsity Tutors</p>
+          <p className="text-xs font-mono">4/2020 - 4/2021</p>
+        </div>
+
         
-        <div className="h-2/3">
+        <div className="h-2/3 mt-3">
           <li>Trained students in calculus and advanced math</li>
           <li>Improved student outcomes via adapted lesson plans and communicated progress to parents</li>
           <li>Used academic coaching skills to help students learn time management and organizational techniques</li>
         </div>
       </div>
 
-      <div className="flex flex-col w-80 border-2 items-center p-4 border-2 rounded-md">
-        <div className="h-1/3 flex items-center">
+      <div className="flex flex-col w-80 items-center p-4 border-2 rounded-md">
+        <div className="h-1/3 flex items-center" style={logoStyle} >
           <img src={require('../assets/progenesis.png')} alt="profile pic of Rachel" height="103px"  width="192px" className=""/>
         </div>
 
-        <div className="basis-2/3">
+        <div className="text-center">
+          <p className="text-2xl font-serif">Laboratory Associate</p>
+          <p className="text-sm font-mono">Progenesis | La Jolla, CA</p>
+          <p className="text-xs font-mono">8/2019 - 3/2020</p>
+        </div>
+
+
+        <div className="basis-2/3 mt-3">
           <li>Aided clinical laboratory scientists in genetic sequencing workflow (PCR and NextGen sequencing) to ensure accuracy of patient results (100-200+ samples/day)</li>
           <li>Extracted DNA from whole blood patient samples to be analyzed for patient-specific assays</li>
           <li>Designed novel packaging with CEO to increase logistical efficiency of embryo biopsy transport</li>
@@ -48,7 +74,14 @@ const WorkExperience = () => {
     </div>
     
 
-
+    <hr align="center" width="5%"
+        className="mt-10"
+        style={{
+            color: '#979797',
+            backgroundColor: '#979797',
+            height: 4
+        }}
+        />
 
 
     </div>

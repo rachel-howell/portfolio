529501a9ec67a7b9b791319a3db16991cc908c09
rachel-howell <rhowell96@gmail.com>
2023-04-20 16:47:35 -0700

    Fix NavBar formatting

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 const NavBar = () => {
   return (
     <div className="flex flex-row md:justify-end justify-between p-3 mt-3 mr-3 w-screen">
-        <div className="hidden md:flex flex-row justify-around w-2/5 items-center text-sm text-[#414141] font-mono">
+        <div className="hidden lg:flex flex-row justify-around w-2/5 items-center text-sm text-[#414141] font-mono">
             <Link href="#home" className="hover:underline">HOME</Link>
             <Link smooth href="#about" className="hover:underline">ABOUT</Link>
             <Link smoooth href="#skills" className="hover:underline">SKILLS</Link>
@@ -19,8 +19,8 @@ const NavBar = () => {
           <MenuIcon fontSize="large"/>
         </div> */}
 
-        <div className="flex flex-row sm:w-1/12 justify-around">
-            <SocialIcon className="sm:mr-0 mr-2" url="https://github.com/rachel-howell" style={{ height: 30, width: 30 }}/>
+        <div className="flex flex-row md:w-1/12 justify-around">
+            <SocialIcon className="md:mr-0 mr-2" url="https://github.com/rachel-howell" style={{ height: 30, width: 30 }}/>
             <SocialIcon url="https://www.linkedin.com/in/racheljhowell/" style={{ height: 30, width: 30 }} bgColor="black" />
         </div>
 

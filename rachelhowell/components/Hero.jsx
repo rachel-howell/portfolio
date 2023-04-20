b6afa8f9e092a536b567d2ba903b6e808e154e57
rachel-howell <rhowell96@gmail.com>
2023-04-20 14:05:20 -0700

    Fix hero buttons; fix top margin of about cat pic

--- rachelhowell/components/Hero.jsx
+++ rachelhowell/components/Hero.jsx
@@ -9,10 +9,11 @@ const Hero = () => {
         <Image src={require('../assets/profilepic.png')} alt="profile pic of Rachel"  className=" mt-36 w-96"/>
         <p className="text-6xl mt-2 text-[#414141] font-serif">Rachel Howell</p>
         <p className="text-2xl mt-4 font-mono text-[#414141]">Full Stack Web Developer</p>
-        <div className="mt-6">
-            <button className="font-mono border-2 mx-2 px-6 py-3 border-[#414141] duration-200 bg-[#414141] text-white hover:bg-white hover:text-[#414141]">My Projects</button>
+        <div className="mt-8">
             
-            <Link href="#contact"><button className="font-mono border-2 mx-2 px-6 py-3 border-[#414141] duration-200 hover:bg-[#414141] hover:text-white">Get in Touch</button></Link>
+            <Link href="#portfolio" className="font-mono border-2 mx-2 px-7 py-4 border-[#414141] duration-200 bg-[#414141] text-white hover:bg-white hover:text-[#414141]">My Projects</Link>
+            
+            <Link href="#contact" className="font-mono border-2 mx-2 px-7 py-4 border-[#414141] duration-200 hover:bg-[#414141] hover:text-white">Get in Touch</Link>
         </div>
 
     </div>

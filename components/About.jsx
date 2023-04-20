bcaf0da31ccc2078e1d507ba9b0c0c57d2066ba7
rachel-howell <rhowell96@gmail.com>
2023-04-20 14:40:56 -0700

    Clean about component

--- components/About.jsx
+++ components/About.jsx
@@ -10,9 +10,6 @@ const About = () => {
         <p className="mt-8 max-w-5xl text-center sm:text-2xl text-lg text-[#414141]">{`I love challenging myself to learn new technologies, and I enjoy using logic to solve complex problems. I'm well-versed in React and Shopify. I am devoted to creating stunning and user-friendly interfaces that elevate the user experience. I am inspired by the dynamic and inventive perspectives of diverse development teams. My goal is to use technology to make the world a better place.`}</p>
         <p className="mt-8 max-w-5xl text-center sm:text-2xl text-lg text-[#414141]">{`In addition to advancing my own skills, I am passionate about giving back to the community. Whether it's volunteering my time to mentor young coders or contributing to open-source projects, I am committed to using my expertise to effect positive change.`}</p>
         <Image src={require('../assets/greyprofile.png')} alt="meow meow" width="100" className=" mt-12 place-self-end md:mr-32 mr-4"/>
-        {/* Icon */}
-        {/* Icon  */}
-        {/* Icon  */}
     </div>
   )
 }

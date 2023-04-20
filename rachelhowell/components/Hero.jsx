0ea2fbc3714a50ab499975fd48a411fddd175807
rachel-howell <rhowell96@gmail.com>
2023-04-20 13:54:44 -0700

    Fix get in touch link button

--- rachelhowell/components/Hero.jsx
+++ rachelhowell/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Hero = () => {
   return (
@@ -10,7 +11,8 @@ const Hero = () => {
         <p className="text-2xl mt-4 font-mono text-[#414141]">Full Stack Web Developer</p>
         <div className="mt-6">
             <button className="font-mono border-2 mx-2 px-6 py-3 border-[#414141] duration-200 bg-[#414141] text-white hover:bg-white hover:text-[#414141]">My Projects</button>
-            <button className="font-mono border-2 mx-2 px-6 py-3 border-[#414141] duration-200 hover:bg-[#414141] hover:text-white">Get in Touch</button>
+            
+            <Link href="#contact"><button className="font-mono border-2 mx-2 px-6 py-3 border-[#414141] duration-200 hover:bg-[#414141] hover:text-white">Get in Touch</button></Link>
         </div>
 
     </div>

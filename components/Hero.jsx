136ac5bfc4245dd0162c1b8f298aafa5fcc16f3c
rachel-howell <rhowell96@gmail.com>
2023-04-21 13:01:11 -0700

    Fix hero button appearance on samsung devices

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,12 +8,16 @@ const Hero = () => {
         {/* Picture of Me */}
         <Image src={require('../assets/profilepic.png')} alt="profile pic of Rachel"  className=" mt-32 sm:w-96 w-72"/>
         <p className="sm:text-6xl text-5xl mt-2 text-[#414141] font-serif">Rachel Howell</p>
-        <p className="text-2xl mt-4 font-mono text-[#414141]">Full Stack Web Developer</p>
-        <div className="mt-8">
+        <p className="text-2xl mt-4 font-mono text-[#414141] text-wrap px-1 text-center">Full Stack Web Developer</p>
+        <div className="mt-5 flex flex-wrap justify-center">
+            <div className="font-mono border-2 mx-2 sm:px-7 px-8 py-4 border-[#414141] duration-200 bg-[#414141] text-white hover:bg-white hover:text-[#414141] mb-2">
+              <Link href="#portfolio">My Projects</Link>
+            </div>
+
+            <div className="font-mono border-2 mx-2 px-7 py-4 border-[#414141] duration-200 hover:bg-[#414141] hover:text-white mb-2">
+              <Link href="#contact" >Get in Touch</Link>
+            </div>
             
-            <Link href="#portfolio" className="font-mono border-2 mx-2 px-7 py-4 border-[#414141] duration-200 bg-[#414141] text-white hover:bg-white hover:text-[#414141]">My Projects</Link>
-            
-            <Link href="#contact" className="font-mono border-2 mx-2 px-7 py-4 border-[#414141] duration-200 hover:bg-[#414141] hover:text-white">Get in Touch</Link>
         </div>
 
     </div>
